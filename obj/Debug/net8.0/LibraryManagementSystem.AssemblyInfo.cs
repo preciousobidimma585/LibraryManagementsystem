a4e2e5774357ddfa55d053cf5475e97a1ecda069
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-02-21 11:05:25 +0100

    wello

--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cadc5eb9bc45e54d60917f9eaa48508fd849fd1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc106dd9cd358c91bb723608b72c9d42ba011244")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
